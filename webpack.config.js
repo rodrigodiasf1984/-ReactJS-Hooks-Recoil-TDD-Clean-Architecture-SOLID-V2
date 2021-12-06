faa0366bc7a11a15a662f26319fb6b88bec3792e
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-12-06 23:21:18 +0100

    chore: config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
-const path = requirte('path');
+const path = require('path');
 const {DefinePlugin} = require('webpack');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/main.index.tsx',
+  entry: './src/main/index.tsx',
   output: {
-    path: path.join(__dirname, 'public/dist'),
+    path: path.join(__dirname, 'public/js'),
     publicPath: '/public/js',
     filename: 'bundle.js',
   }, 
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.scss'],
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.join(__dirname, 'src'),
     }
   }, 
   module: {
