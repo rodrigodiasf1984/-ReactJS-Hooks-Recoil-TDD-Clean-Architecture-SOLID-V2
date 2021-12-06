e8d75e7c24db3522892dc21a6cf145222b23f882
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-12-06 23:54:10 +0100

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const {DefinePlugin} = require('webpack');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { DefinePlugin } = require('webpack')
 
 module.exports = {
   mode: 'development',
@@ -8,51 +8,58 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public/js'),
     publicPath: '/public/js',
-    filename: 'bundle.js',
-  }, 
+    filename: 'bundle.js'
+  },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.scss'],
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
-      '@': path.join(__dirname, 'src'),
+      '@': path.join(__dirname, 'src')
     }
-  }, 
+  },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.ts(x?)$/,
         loader: 'ts-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       {
         test: /\.scss$/,
-        use: 
-        [
+        use: [
+          {
+            loader: 'style-loader'
+          },
           {
             loader: 'css-loader',
             options: {
-              modules: true,
+              modules: true
             }
           },
           {
-            loader: 'sass-loader',
+            loader: 'sass-loader'
           }
         ]
       }
     ]
-  }, 
+  },
   devServer: {
-    contentBase: 'public',
-    writeToDisk: true,
+    devMiddleware: {
+      writeToDisk: true
+    },
+    static: {
+      directory: './public'
+    },
     historyApiFallback: true,
-  }, 
+    port: 8080
+  },
   externals: {
     react: 'React',
-    'react-dom': 'ReactDOM',
-  }, 
+    'react-dom': 'ReactDOM'
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new DefinePlugin({
-      'process.env.API_URL': JSON.stringify('http://fordevs.herokuapp.com/api'),
-    }),
+      'process.env.API_URL': 'http://fordevs.herokuapp.com/api'
+    })
   ]
 }
